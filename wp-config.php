11978c52e61ff936bec8771824feaad1c2364099
Viral Solani <viral.solani@gmail.com>
2019-07-30 18:40:51 +0530

    change again

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@
 define ('CONCATENATE_SCRIPTS', false );
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'etoile');
+define('DB_NAME', 'wp_etoile');
 
 /** MySQL database username */
 define('DB_USER', 'root');
