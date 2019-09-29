2234929a1de1fa92f3c2352a2135fc01cdcd519e
Viral Solani <viral.solani@gmail.com>
2019-09-29 18:40:21 +0530

    update wp-config file

--- wp-config.php
+++ wp-config.php
@@ -65,6 +65,9 @@ define( 'NONCE_SALT',       'R^o)1%qvA+5i0cNk&`?cJ=k{;#0qg26e]q|]g>~[Yp7;brImV5$
  */
 $table_prefix = 'wp_';
 
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
+
 /**
  * For developers: WordPress debugging mode.
  *
@@ -90,5 +93,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
 }
 
-/** Sets up WordPress vars and included files. */
-require_once( ABSPATH . 'wp-settings.php' );
+
