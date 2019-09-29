e8c20e00bab5870eb045055a3dec98397d0601db
Viral Solani <viral.solani@gmail.com>
2019-09-29 18:28:52 +0530

    change in wp-config file

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'etoilefashion_new' );
+define( 'DB_NAME', 'wp_etoile' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'August@2019' );
 
 /** MySQL hostname */
 define( 'DB_HOST', 'localhost' );
